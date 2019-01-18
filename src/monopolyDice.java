89d6e322210f0857acc1ba4073be36fadb48686d
Jennifer Lee <jennifer.jlee@outlook.com>
2019-01-18 15:08:07 -0500

    Changed the while loop to do-while loop. Corrected the option to play again. Added system exit command at the end.

--- src/monopolyDice.java
+++ src/monopolyDice.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 
 public class monopolyDice {
     public static void main(String[] args){
+
         int diceA = 0;
         int diceB = 0;
         int doubleRollCount = 0;
-        String quitNow = " ";
+        String playAgain = "y";
 
         Random rnd = new Random();
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Would you like to quit the game at this time?: Y or N");
-        quitNow = keyboard.nextLine();
 
         // roll two dices and show results
-        while(quitNow.toLowerCase() != "y"){
+        do {
             diceA = 1 + (rnd.nextInt(6));
             diceB = 1 + (rnd.nextInt(6));
             System.out.println("User rolls " + diceA + " & " + diceB);
@@ -24,16 +23,55 @@ public class monopolyDice {
                 doubleRollCount++;
 
                 if(doubleRollCount < 3){
-                    continue;
-                }else
-                    System.out.println("GO TO JAIL ---> bye");
+                    System.out.println("Move " + (diceA + diceB) + " spaces and roll again getting...");
+                    //continue;
+                }else{
+                    System.out.println("GO TO JAIL --->");
+                    break;
+                }
 
-            }else
+            }else{
                 System.out.println("Move " + (diceA + diceB) + " spaces and stop");
-                break;
+            }
+
+
+            playAgain = " ";
+            System.out.println("Would you like to roll the dice again?: Y or N");
+            playAgain = keyboard.nextLine();
         }
-        System.out.println("Would you like to quit the game at this time?: Y or N");
-        quitNow = keyboard.nextLine();
+        while(playAgain.equalsIgnoreCase("y"));
+
+        // if user does not want to play again
+        // display exit message
+        System.out.println("You are now leaving the game. Goodbye!");
+        keyboard.close();
+        System.exit(0);
+
+
+//        while(playAgain.toLowerCase() == "y"){
+//            diceA = 1 + (rnd.nextInt(6));
+//            diceB = 1 + (rnd.nextInt(6));
+//            System.out.println("User rolls " + diceA + " & " + diceB);
+//
+//            if(diceA == diceB){
+//                System.out.println("DOUBLES!");
+//                doubleRollCount++;
+//
+//                if(doubleRollCount < 3){
+//                    System.out.println("Move " + (diceA + diceB) + " spaces and roll again getting...");
+//                    //continue;
+//                }else
+//                    System.out.println("GO TO JAIL ---> bye");
+//
+//            }else
+//                System.out.println("Move " + (diceA + diceB) + " spaces and stop");
+//                break;
+//        }
+
+//        System.out.println();
+//        playAgain = "";
+//        System.out.println("Would you like to play another game at this time?: Y or N");
+//        playAgain = keyboard.nextLine();
 
 
 
